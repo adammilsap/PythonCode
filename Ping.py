cb457a8ab64540f9a3b0f36678dc79c21b22f6e5
adammilsap <79421660+adammilsap@users.noreply.github.com>
2021-02-21 16:51:39 -0600

    Add files via upload
    Ping a range of IP addresses

---
+++ Ping.py
@@ -0,0 +1,15 @@
+# %%
+import subprocess
+import os
+a = int(input("The range of addresses you want scanned lower: "))
+b = int(input("The range of addresses you want scanned higher: "))
+with open(os.devnull, "wb") as limbo:
+        for n in range(a, b):
+                ip="10.0.0.{0}".format(n)
+                result=subprocess.Popen(["ping", "-n", "1", "-w", "1", ip],
+        
+                        stdout=limbo, stderr=limbo).wait()
+                if result:
+                        print (ip + " Inactive")
+                else:
+                        print (ip + " Active")
